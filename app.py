524765a9bb0db49683208e05b2fc84c827560156
jairomart123 <jairomart123@hotmail.com>
2022-02-16 20:54:25 -0500

    mi primer cambio

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 
 var_app = Flask(__name__)
 
@@ -10,7 +10,8 @@ def inicio():
 @var_app.route("/cur")
 
 def cur():
-    return"Bienvenido a la univercidad"
+    return render_template(froned/Cur.html)
+    
 
 if __name__ == "__main__":
     var_app.run(debug=True,port=3000)
