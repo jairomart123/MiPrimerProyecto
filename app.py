9f6a404d905acc57cecd083046d71362bb91dea9
jairomart123 <jairomart123@hotmail.com>
2022-02-21 17:15:44 -0500

    cambio archivos de carpeta

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask , render_template,redirect,url_for
+# save this as app.py
+from flask import Flask , render_template
+
 
 var_app = Flask(__name__)
 
-@var_app.route("/")
-
-def inicio():
+@var_app.route('/')
+def hello():
     return"Hola mundo"
-
-@var_app.route("/cur")
-
-def cur():
-    return render_template("/fronet/cur.hmtl")
     
+
+@var_app.route('/cur')
+def cur():
+    
+    return render_template("index.html")
+
+
+if __name__ == "__main__" :
+    var_app.run(host='0.0.0.0',port=3000,debug=True,)
+
+"""
 
 if __name__ == "__main__" :
     var_app.run(debug=True,port=3000)
 
-##if __name__ == "__main__" :
-    ##var_app.run(host='0.0.0.0',port=3000,debug=True,)
+"""
