913b8e41f8f7f2117e01e767232109804235c695
jairomart123 <jairomart123@hotmail.com>
2022-03-03 08:57:40 -0500

    nueva pagina

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
 # save this as app.py
-from flask import Flask , render_template,url_for
-
+from flask import Flask, render_template, url_for
 
 
 var_app = Flask(__name__)
 
-@var_app.route('/')
-def inicio():
-    return render_template('layout.html')
-    
-@var_app.route('/home')
-def home():
-    return render_template('home.html')
 
-@var_app.route('/productos')
+@var_app.route("/")
+def inicio():
+    return render_template("layout.html")
+
+
+@var_app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@var_app.route("/productos")
 def productos():
     return render_template("productos.html")
-   ### return render_template("index.html")
 
-@var_app.route('/contactos')
+
+### return render_template("index.html")
+
+
+@var_app.route("/contactos")
 def contactos():
     return render_template("contactos.html")
-   ### return render_template("index.html")
-@var_app.route('/q_somos')
+
+
+### return render_template("index.html")
+@var_app.route("/q_somos")
 def q_somos():
     return render_template("q_somos.html")
-   ### return render_template("index.html")
 
-@var_app.route('/ptotal')
+
+### return render_template("index.html")
+
+
+@var_app.route("/ptotal")
 def ptotal():
-    return render_template('pTotal.html')
-
-if __name__ == "__main__" :
-    var_app.run(host='0.0.0.0',port=3000,debug=True,)
+    return render_template("pTotal.html")
 
 
+if __name__ == "__main__":
+    var_app.run(
+        host="0.0.0.0",
+        port=3000,
+        debug=True,
+    )
+
+css = {{url_for("static", filename="css/*.")}}
+
+img = {{url_for("static", filename="img/*.")}}
+js = {{url_for("static", filename="js/*.")}}
+video = {{url_for("static", filename="video/*.")}}
 
 
 """
